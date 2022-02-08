cfe187df2830e83dd41f00f8af1c67e8cb26777d
mohamadsajjadi <alisajadian49@yahoo.com>
2022-02-08 19:05:58 +0330

    well done

---
+++ Advance Python/defferent letter.py
@@ -0,0 +1,22 @@
+# input
+# 4
+# ali
+# karim
+# abbas
+# mohammad
+
+# result
+# 5
+# Print a number on the only output line that is equal to the number of different letters in the selected name.
+
+n = int(input())
+max = 0
+for i in range(n):
+    t = ''
+    my_input = input()
+    for char in my_input:
+        if char not in t:
+            t += char
+    if len(t) > max:
+        max = len(t)
+print(max)
