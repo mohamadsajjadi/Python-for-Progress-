d1fe529d7990cae121185cc61022f7dd5b175a51
mohamadsajjadi <alisajadian49@yahoo.com>
2022-02-16 11:39:35 +0330

    completed count lines !

---
+++ Advance Python/count lines.py
@@ -0,0 +1,14 @@
+def solve(path):
+    path1 = open(path, 'r')
+    lst = list()
+    counter = 0
+    for lines in path1:
+        lst.append(lines.strip())
+    for item in lst:
+        if not item.startswith('#') and item != '':
+            counter += 1
+    return counter
+
+
+path = open('info.txt', 'r')
+print(solve(path))
